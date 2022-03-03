960dc6cde0f7f785c9e88ab138862d3909c25735
DianBogda <diana.bogdanic@gmail.com>
2022-03-03 11:58:57 -0300

    Proyecto erróneo

--- Proyecto_terminado/server/config/mongoose.config.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/pestañas_spa_db', {})
-    .then(res => console.log('Conexión con base de datos exitosa'))
-    .catch(res =>console.log('No se pudo conectar a la base de datos'))
